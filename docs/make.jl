b8f93976084bc9083dff7e85fdf1c1a649ae01f5
Masoud Barati <87744817+msdbarati@users.noreply.github.com>
2023-02-27 16:12:30 -0500

    Add files via upload

---
+++ docs/make.jl
@@ -0,0 +1,44 @@
+using Documenter, PowerModels
+
+makedocs(
+    modules = [PowerModels],
+    format = Documenter.HTML(analytics = "UA-367975-10", mathengine = Documenter.MathJax()),
+    sitename = "PowerModels",
+    authors = "Carleton Coffrin, Russell Bent, and contributors.",
+    pages = [
+        "Home" => "index.md",
+        "Manual" => [
+            "Getting Started" => "quickguide.md",
+            "Network Data Format" => "network-data.md",
+            "Result Data Format" => "result-data.md",
+            "Mathematical Model" => "math-model.md",
+            "Power Flow" => "power-flow.md",
+            "Storage Model" => "storage.md",
+            "Switch Model" => "switch.md",
+            "Multi Networks" => "multi-networks.md",
+            "Utilities" => "utilities.md",
+            "Basic Data Utilities" => "basic-data-utilities.md"
+        ],
+        "Library" => [
+            "Network Formulations" => "formulations.md",
+            "Problem Specifications" => "specifications.md",
+            "Modeling Components" => [
+                "PowerModel" => "model.md",
+                "Objective" => "objective.md",
+                "Variables" => "variables.md",
+                "Constraints" => "constraints.md"
+            ],
+            "Relaxation Schemes" => "relaxations.md",
+            "File IO" => "parser.md"
+        ],
+        "Developer" => [
+                "Developer" => "developer.md",
+                "Formulation Details" => "formulation-details.md"
+        ],
+        "Experiment Results" => "experiment-results.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/lanl-ansi/PowerModels.jl.git",
+)
